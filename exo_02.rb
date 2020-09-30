4bee929f93a1046529b7f68c146588a89fe67633
Chaneze <chaneze.aroua@gmail.com>
2020-09-30 03:03:01 +0200

    correction

--- exo_02.rb
+++ exo_02.rb
@@ -1,2 +1,3 @@
 puts "Bonjour, monde !"
 puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+# print n'affiche pas de retour a la ligne alors que puts si. Ainsi meme si on fait plusieurs print, ils seront les uns derriere les autres
