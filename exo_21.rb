747dd33c9ee8588581c8d82ada2d3ab5faa21b47
Chaneze <chaneze.aroua@gmail.com>
2020-09-30 03:14:39 +0200

    pyramide

--- exo_21.rb
+++ exo_21.rb
@@ -2,6 +2,7 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 user_number = gets.chomp.to_i
 if user_number >= 1 && user_number <= 25
+	puts "Voici la pyramide :"
 	(user_number).times do |i|
 		(user_number - i - 1).times do
 			print " "
